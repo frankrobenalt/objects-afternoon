2c95f28fe5d6a991d7e4ee0d194ac06c22bf4745
Frank Robenalt <frank.robenalt@gmail.com>
2017-08-28 17:08:43 -0500

    complete

--- objects.js
+++ objects.js
@@ -2,42 +2,58 @@
 
 // Code Here
 
-
+var user = {
+    name: 'Frank',
+    age: 24,
+    password: 'hhhhhhh'
+}
 
 // Add an email and username, as well as a method called alertUser that alerts the users name, to your user object using dot notation.
 
 // Code Here
+user.email = 'frank@frink.frunk';
+user.username = 'frankie42forearms';
+user.alertUser = function(){alert(this.name)};
 
 
 
 // Now loop over your user object and alert each of the keys.
 
 // Code Here
-
+for (var prop in user) {
+    alert(prop);
+}
 
 
 // Now loop over your user object and alert each of the values for each key.
 
 // Code Here
-
+for (var prop in user) {
+    alert(user[prop]);
+}
 
 
 // Remove the username from your user object
 
 // Code Here
 
-
+delete user.username;
 
 // Create an object called creditCard that has number, cvc, type, and limit properties and assign them appropriate values.
 
 // Code Here
 
-
+var creditCard = {
+    number: 123456789,
+    cvc: 345,
+    type: moostoocood,
+    limit: 10000000000000
+}
 
 // Create a variable called alternating. Set it equal to the string bonusCategories.
 
 // Code Here
-
+var alternating = 'bonusCategories';
 
 
 /* Using bracket notation, add a bonusCategories key to your creditCard object using 'alternating'.
@@ -52,13 +68,23 @@
     */
 
 // Code Here
+creditCard[alternating] = [{
+    Department Stores: '5%',
+},{
+    grocery stores: '4%'
+},{
+    gas stations: '6%'
+}];
 
+creditCard.bank = {'bank name': 'PNC', 'address': '123 Bank Rd'};
 
 
 // Write a function that will take your creditCard object and user object as arguments and combine them into one object. /* HINT: You will need a for..in loop */
 
 // Code Here
-
+for (var key in user) {
+    creditCard[key] = user[key];
+}
 
 /*Below you are given an object. Write a function that will loop over the allies property, remove LittleFinger, and insert Dragon Lady.
     It will also change alive to 'More Than Ever'.
@@ -71,5 +97,10 @@ var obj = {
   };
 
   // Code Here
-
+function littleFingies (ob) {
+    for (var key in ob) {
+      ob[key][2] = 'Dragon Lady';
+    }
+    ob.alive = 'More Than Ever';
+}
   
